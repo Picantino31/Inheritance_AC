e6e1a8d0d627a523f1f72840cf81f5ee9fd10aaa
Picantino31 <Aaroncheng1611@gmail.com>
2024-12-05 15:12:21 -0700

    a

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import Ex1_Animals.Ex1_Main;
+import Ex2_Restaurants.Ex2_Main;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Inheritance Project");
+        Ex2_Main.run();
+    }
+
+}
