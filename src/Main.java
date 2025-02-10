4a8a50f9cf3eb2f6ff5554fc6d74b5fda37a2cae
Picantino31 <Aaroncheng1611@gmail.com>
2025-02-10 15:25:34 -0700

    a

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import Ex1_Animals.Ex1_Main;
 import Ex2_Restaurants.Ex2_Main;
 import Ex3_MusicFestival.Ex3_Main;
 import TwoDArrays.As1_Bingo;
+import TwoDArrays.As2_Vigenere;
 import TwoDArrays.Ex1_GolfCard;
 import TwoDArrays.Ex2_Tic;
 
@@ -10,7 +11,8 @@ public class Main {
 
         System.out.println("Inheritance Project");
 //        Ex2_Main.run();
-        As1_Bingo.run();//        Ex2_Tic.run();
+//        As1_Bingo.run();//
+        As2_Vigenere.run();//        Ex2_Tic.run();
     }
 
 }
